cb5f015568159b5348a4161a19cf7df78255dea4
myfernandez <myfernandez@gmail.com>
2018-03-26 07:04:01 -0700

    update with more examples

--- numpy_array_examples.py
+++ numpy_array_examples.py
@@ -42,3 +42,109 @@ z
 
 np.shape(z)
 
+
+# In[17]:
+
+
+linear = np.linspace(0,4,9)
+print linear
+
+
+# In[18]:
+
+
+print linear.reshape(3,3)
+
+
+# In[19]:
+
+
+print linear.resize(3,3)
+
+
+# In[20]:
+
+
+print linear
+
+
+# In[21]:
+
+
+print z.resize(2,3)
+
+
+# In[22]:
+
+
+print z.resize(3,2)
+
+
+# In[23]:
+
+
+print z
+
+
+# In[25]:
+
+
+np.ones((3,2))
+
+
+# In[26]:
+
+
+np.zeros((2,2))
+
+
+# In[27]:
+
+
+np.diag(y)
+
+
+# In[30]:
+
+
+np.repeat([1,2,3],3)
+
+
+# In[31]:
+
+
+np.array([1,2,3]*3)
+
+
+# In[32]:
+
+
+p=np.ones([2,3],int)
+p
+
+
+# In[33]:
+
+
+np.array(p*2)
+
+
+# In[34]:
+
+
+#notice the difference between operations in a list and array
+#for example [1,2,3]*3 is different than array([1,2,3])*3
+
+
+# In[35]:
+
+
+np.array([1,2,3])*3
+
+
+
+# In[36]:
+
+
+np.array([1,2,3]*3)
+
